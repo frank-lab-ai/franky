b23a829c7b4f8f28aff911c021fb72ab8c656c87
Kobby Nuamah <nkobby@gmail.com>
2020-11-04 00:17:41 +0000

    option to run multiple queries from a file for evaluation

--- frank_cli.py
+++ frank_cli.py
@@ -23,15 +23,20 @@ inference_graphs = {}
 argparser =  argparse.ArgumentParser(
         prog="FRANK CLI", 
         description="Command line interface for FRANK")
-argparser.add_argument("-q", "--query", metavar='Q', type=str, 
-    default="", help='a query to be answered; a text or alist string query')
-argparser.add_argument("-c", "--context", metavar='C', type=str,
-    default={}, help='context for the query; (default = {})',
-        )
+argparser.add_argument("-q", "--query", type=str, 
+    default="", help="a query to be answered; a text or alist string query")
+argparser.add_argument("-c", "--context", type=str,
+    default={}, help="context for the query; (default = {})")
+argparser.add_argument("-f", "--file", type=str,
+    help="batch file; used when evaluating multiple questions in a file")
+argparser.add_argument("-o", "--output", type=str,
+    default="output.json", help="file to output batch query results to; (default = output.json)")
+
 
 def cli(query="", context={}):
     session_id = uuid.uuid4().hex
     interactive = False
+    answer = None
     if query:
         query_json = None
     else:
@@ -52,7 +57,7 @@ def cli(query="", context={}):
     if query_json:        
         alist = Alist(**query_json)
         if context:
-            context_json = json.loads(context)
+            context_json = json.loads(context) if type(context) == 'str' else context
             alist.set(tt.CONTEXT, context_json)
         print(f"{pcol.YELLOW} ├── query alist:{json.dumps(alist.attributes)} {pcol.RESETALL}")
         print(f"{pcol.YELLOW} └── session id:{session_id} {pcol.RESETALL}\n")
@@ -60,15 +65,39 @@ def cli(query="", context={}):
         launch.start(alist, session_id, inference_graphs)
 
         if session_id in inference_graphs:
-            graph:InferenceGraph = inference_graphs[session_id]['graph']
+            answer = inference_graphs[session_id]['answer']['answer']
+            graph = inference_graphs[session_id]['graph']
             graph.plot_plotly(query)
         if interactive:
             input("\npress any key to exit")
     else:
         print("\nCould not parse question. Please try again.")
+    return answer
+
+def batch(batch_file, output):
+    results = []
+    with open(batch_file) as json_file:
+        queries = json.load(json_file)
+        for q in queries:
+            answer = cli(q['question'], q['context'])
+            results.append({'id': q['id'], 'answer': answer})
+            with open(output, 'w') as out_file:
+                json.dump(results, out_file)
+    
+    
+    
+    print('Done! \nResults in ' + output)
 
 
 if __name__ == '__main__':
     args = argparser.parse_args()
-    cli(args.query, args.context)
+    if args.file and args.query:
+        print("\nThe --query option cannot be used with the --file option.")
+    if args.file and args.context:
+        print("\nCannot use --context together with the --file flag.")  
+
+    if args.file:
+        batch(args.file, args.output)
+    else:
+        cli(args.query, args.context)
  
