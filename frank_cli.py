b609add1cce6f23ed65f723093b9673eafc86502
njf0 <nickferguson107@icloud.com>
2023-09-18 20:52:28 +0100

    Added option to 'save query' (capture stdout)

--- frank_cli.py
+++ frank_cli.py
@@ -4,8 +4,13 @@ Description: REST-based API for FRANK
 '''
 
 import argparse
+import contextlib
+import datetime
+import io
 import json
+import re
 import uuid
+from pathlib import Path
 
 import frank.explain
 import frank.query_parser.parser
@@ -13,7 +18,6 @@ from frank.alist import Alist
 from frank.alist import Attributes as tt
 from frank.launcher import Launcher
 from frank.processLog import pcolors as pcol
-
 
 inference_graphs = {}
 argparser =  argparse.ArgumentParser(
@@ -27,7 +31,8 @@ argparser.add_argument("-f", "--file", type=str,
     help="batch file; used when evaluating multiple questions in a file")
 argparser.add_argument("-o", "--output", type=str,
     default="output.json", help="file to output batch query results to; (default = output.json)")
-
+argparser.add_argument("-s", "--save-query", type=bool,
+    default=False, help="If True, saves alist under ./logs/timestamp.json")
 
 def cli(
         query: str | dict,
@@ -86,8 +91,8 @@ def cli(
         if interactive:
             input("\npress any key to exit")
     else:
-        print("\nCould not parse question. Please try again.")
-    return answer
+        raise ValueError("Could not parse question. Please try again.")
+    return answer, session_id, query_json, alist.attributes
 
 def batch(
         batch_file: str,
@@ -113,6 +118,52 @@ def batch(
 
     print('Done! \nResults in ' + output)
 
+def save_query(
+        timestamp: str,
+        session_id: str,
+        query_nl: str,
+        query_alist: dict,
+        graph: list,
+        answer_nl: str,
+        answer_alist: dict,
+        ) -> dict:
+    """Save information about a Frank query.
+    
+    Parameters
+    ----------
+    timestamp: str
+        Timestamp of session as returned by datetime.datetime.now().isoformat()
+    session_id: str
+        UUID token created for the query.
+    query_nl: str
+        The natural language query, e.g., "What will the population of France be in 2029?"
+    query_alist: json
+        The natural language query passed to alist form.
+    graph: list
+        The output of Frank's inference process. Is converted to leave only intermediate alists.
+    answer_nl: str,
+        The natural language answer, e.g., 62333000.
+    answer_alist: dict
+        The answer to the query in alist form.
+    """
+    # Save only alists from query
+    regex = re.compile(r'^.*?(\{.*\}).*')
+    matches = [m.group(1) for m in filter(None, map(regex.search, graph))]
+    corrected_output = [i.replace("\'", '"').replace('"{', '{').replace('}"', '}') for i in matches]
+    output_as_json = [json.loads(i) for i in corrected_output]
+
+    output = {'timestamp': timestamp, 'session_id': session_id, 'query_nl': query_nl,
+              'query_alist': query_alist, 'graph': output_as_json,
+              'answer_nl': answer_nl, 'answer_alist': answer_alist}
+
+    output_dir = Path('logs')
+    if not output_dir.exists():
+        output_dir.mkdir()
+    with open(output_dir / f'{timestamp}.json', 'w', encoding='utf-8') as outfile:
+        json.dump(output, outfile, indent=4)
+
+    return output
+
 
 if __name__ == '__main__':
     args = argparser.parse_args()
@@ -121,7 +172,18 @@ if __name__ == '__main__':
     if args.file and args.context:
         print("\nCannot use --context together with the --file flag.")
 
+    query_timestamp = datetime.datetime.now().isoformat()
     if args.file:
         batch(args.file, args.output)
     else:
-        cli(args.query, args.context)
+        if args.save_query:
+            # HACK: Instead of propagating output through class methods, capture stdout lines into
+            #       a string buffer.
+            stdoutput = io.StringIO()
+            with contextlib.redirect_stdout(stdoutput):
+                ans, s_id, q_alist, a_alist = cli(args.query, args.context)
+            formatted_output = stdoutput.getvalue().splitlines()
+            save_query(query_timestamp, s_id, args.query,
+                       q_alist, formatted_output, ans, a_alist)
+        else:
+            cli(args.query, args.context)
